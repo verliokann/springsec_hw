486509e465a7ae8d7ba98e7eba35d18a1d8be2ed
zhevnerchuk <zhevnerchuk@yandex.ru>
2024-01-06 08:05:39 +0300

    Введение в Spring Security

---
+++ src/test/java/ru/nnov/nntu/irit/springsecuritydemo/SpringsecuritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nnov.nntu.irit.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
